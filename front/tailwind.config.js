76f382a3a7f89cf2bd34cf5cca68a057db98d883
793150Zivi <Zivi793150@yandex.ru>
2026-02-18 17:30:18 +0300

    fix: tailwind v3 for vercel

---
+++ front/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./sections/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: ["class", '[data-theme="dark"]'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
